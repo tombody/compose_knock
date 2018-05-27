2ac390c3e93f723b3b86463f68962967fc987f7b
Alejandro Diaz Gonzalez Beltran <alejandrodgb@gmail.com>
2018-05-27 04:38:58 +0000

    make test train file

---
+++ train_test.py
@@ -0,0 +1,18 @@
+#! /usr/bin/python
+
+import random
+import os
+
+def make_test_train(l):
+
+    output = open(out_f, 'w')
+    infile = open(in_f, 'rb)
+        
+
+
+
+
+
+
+if __name__ == '__main__':
+    pass
